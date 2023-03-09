985493a650f24542ee597f8746c4fb24134e076b
VinOne24 <al24vin@gmail.com>
2023-03-09 10:58:49 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SkodiController;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,6 +23,13 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+
+// Route::get('/home', [SkodiController::class, 'home'])->name('home');
+
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
