21802ef96bd637549b4b43629b842c5ff539eee1
Adhitya Hasan <adhityahasan011@gmail.com>
2023-03-09 16:21:14 +0700

    update dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,25 @@
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     
 
-                <img class="float-left " src="path/to/image.jpg">
-                <p>Maybe we can live without libraries, people like you and me. ...</p>
-
+                <div class="grid grid-cols-2 gap-2">
+                    <div class="float-left ">
+                    <img  src="{{ asset('/assets/img/orang.png') }}">
+                    </div>
+                    <div class="text-5xl font-bold font-poppins py-9 leading-snug">
+                        <h5>Naikkan potensi digital <br>skill untuk masa depan <br>lebih baik</br></h5>
+                        <h3 class="text-base font-poppins font-light py-9 leading-normal">Tingkatkan kemampuan digital kamu untuk <br>Indonesia mulai gerakan<br>
+                        <span><h3 class="text-base font-poppins font-bold ">#digitalisasibangsa</h3></span></h3>
+                        
+                        
+                    </div>
+                <!-- <div class="text-base font-poppins leading-snug">
+                    
+                </div>
+                <div class="text-base font-poppins font-bold leading-snug">
+                    
+                </div>
+                </div> -->
+                
 
                 </div>
             </div>
