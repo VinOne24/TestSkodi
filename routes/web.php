12cc94847d62dc16c4acea18173792ee57bde062
Adhitya Hasan <adhityahasan011@gmail.com>
2023-03-14 13:31:33 +0700

    route

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
 
 // Route::get('/dass', function () {
 //     return view('dass');
 // });
-Route::get('/dass', [SkodiController::class, 'dass'])->name('dass');
+Route::get('/', [SkodiController::class, 'dass'])->name('dass');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
